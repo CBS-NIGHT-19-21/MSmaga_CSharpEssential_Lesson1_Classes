39ad21696ee2a609c5c5abc2d8e7c589cb256ca1
MSmaga <msmaga@protonmail.com>
2022-01-31 23:15:20 +0200

    Encapsulate method Show();

--- Task_Additional_Address/Program.cs
+++ Task_Additional_Address/Program.cs
@@ -5,22 +5,22 @@
         #region Задание Дополнительное
 
         // Создать класс с именем Address. 
-        // В теле класса требуется создать поля: index, country, city, street, house, apartment. Для каждого 
-        // поля, создать свойство с двумя методами доступа. 
+        // В теле класса требуется создать поля: index, country, city, street, house, apartment.
+        // Для каждого поля, создать свойство с двумя методами доступа. 
         // Создать экземпляр класса Address. 
-        // В поля экземпляра записать информацию о почтовом адресе. Выведите на экран значения полей, описывающих адрес. 
+        // В поля экземпляра записать информацию о почтовом адресе. Выведите на экран значения полей,
+        // описывающих адрес. 
         #endregion
 
         #region Address - Encapsulation
-
         // Instantiate (create) an instance of address of the Address class.
-        // Get a reference to the address of the instance on the heap (Heap) by calling the default constructor.
-
+        // Get a reference to the address of the instance on the heap (Heap)
+        // by calling the default constructor.
         #endregion
 
         Address address = new()
         {
-            Index = "063",
+            Index = "063", 
             Country = "Ukraine",
             City = "Kyev",
             Street = "Svobody",
@@ -29,8 +29,7 @@
         };
 
         // Show the auto properties of the instance.
-        Console.WriteLine($"Index = {address.Index}, Country = {address.Country}, City = {address.City}, " +
-                          $"Street = {address.Street}, House = {address.House}, Apartment = {address.Apartment}");
+        address.Show();
 
 
         // Delay.
